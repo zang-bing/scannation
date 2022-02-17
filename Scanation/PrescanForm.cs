ed2f69686d4f180a3e365138b70dd159bdf0d32f
binh.buidang <dangbinh4869@gmail.com>
2022-02-17 23:20:28 +0700

    feat: pre-scan.

---
+++ Scanation/PrescanForm.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Scanation
+{
+    public partial class PrescanForm : Form
+    {
+        private List<Bitmap> _images;
+        private int _imageIndex = -1;
+
+        public PrescanForm(List<Bitmap> images)
+        {
+            InitializeComponent();
+            _images = images;
+        }
+
+        private void PrevBtn_Click(object sender, EventArgs e)
+        {
+            if (_imageIndex > 0)
+            {
+                pictureBox.Image = _images[--_imageIndex];
+                nextBtn.Enabled = true;
+            }
+            if (_imageIndex <= 0)
+            {
+                prevBtn.Enabled = false;
+            }
+        }
+
+        private void NextBtn_Click(object sender, EventArgs e)
+        {
+            if (_imageIndex < _images.Count - 1)
+            {
+                pictureBox.Image = _images[++_imageIndex];
+                prevBtn.Enabled = true;
+            }
+            if (_imageIndex >= _images.Count - 1)
+            {
+                nextBtn.Enabled = false;
+            }
+        }
+
+        private void PrescanForm_Load(object sender, EventArgs e)
+        {
+            if (_images != null && _images.Count > 1)
+            {
+                nextBtn.Enabled = true;
+                _imageIndex = 0;
+                pictureBox.Image = _images[_imageIndex];
+            }
+        }
+    }
+}
