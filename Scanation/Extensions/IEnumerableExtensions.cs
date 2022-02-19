99750a43c6a42202ff23d5a7bbaf5c7ad9c876fc
binh.buidang <dangbinh4869@gmail.com>
2022-02-19 23:57:17 +0700

    add: extension methods.

---
+++ Scanation/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Scanation.Utils.FaceUtils
+{
+    public static class IEnumerableExtensions
+    {
+        public static IEnumerable<T> HasElements<T>(this IEnumerable<T> array, Action action)
+        {
+            if (array.Any())
+                action();
+            return array;
+        }
+        public static void ForEach<T>(this IEnumerable<T> array, Action<T> action)
+        {
+            foreach (var item in array)
+                action(item);
+        }
+    }
+}
---
+++ Scanation/Extensions/PictureBoxExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace Scanation.Extensions
+{
+    public static class PictureBoxExtensions
+    {
+        public static void DrawRectangle(this PictureBox pictureBox, Rectangle rectangle, Color color, float width)
+        {
+            using (var g = pictureBox.CreateGraphics())
+            {
+                g.DrawRectangle(new Pen(new SolidBrush(color), width), rectangle);
+            }
+        }
+    }
+}
